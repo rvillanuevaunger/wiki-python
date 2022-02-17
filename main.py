755982e454dcf015bbae1cb96685bedcdd3d14ea
Reina Villanueva-Unger <81713572+rvillanuevaunger@users.noreply.github.com>
2022-02-17 13:36:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,19 +18,19 @@ stop_words = list(get_stop_words('en'))
 
 #For each section identify titles, total word counts, and links
 
-for header in soup.find_all(['h1', 'h2', 'h3']):  # 1.
-    title = header.get_text()  # 2.
-    title = title[:-6]  # 3.
-    print(title)  # 4.
-    for elem in header.next_siblings:  # 5.
+for header in soup.find_all(['h1', 'h2', 'h3']): 
+    title = header.get_text()  
+    title = title[:-6]  
+    print(title)  
+    for elem in header.next_siblings:  
         if elem.name and elem.name.startswith('h'):
             break
-        if elem.name in ('p', 'ul'):  # 6.
-            text = elem.get_text()  # 7.
-            words = re.findall(r'\b\w+', text)  # 8.
-            lower_case = Counter([word.lower() for word in words if not word.lower() in stop_words])  # 9.
-            df = pd.DataFrame(lower_case.most_common(5)) # 10.
-            df.columns = ['words', 'frequency']  # 11.
-            print(df)  # 12.
-            for links in elem.find_all('a'):  # 13.
-                print(links['href'])  # 14.
+        if elem.name in ('p', 'ul'): 
+            text = elem.get_text()  
+            words = re.findall(r'\b\w+', text)  
+            lower_case = Counter([word.lower() for word in words if not word.lower() in stop_words])  
+            df = pd.DataFrame(lower_case.most_common(5)) 
+            df.columns = ['words', 'frequency']  
+            print(df)  
+            for links in elem.find_all('a'): 
+                print(links['href'])  
